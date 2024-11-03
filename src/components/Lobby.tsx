004ceebaa7ad934bfd97eb5788053e98d88c9d40
BafometSatC <prudnikovdiablo@gmail.com>
2024-11-03 23:36:05 +0300

    test commit111

--- src/components/Lobby.tsx
+++ src/components/Lobby.tsx
@@ -55,7 +55,7 @@ const Lobby = () => {
                 id="link"
                 value={joinLink}
                 onChange={(e) => setJoinLink(e.target.value)}
-                placeholder="Вставьте ссылку или ID комнаты"
+                placeholder="Вставьте TEST TEST TEST ссылку или ID комнаты"
                 className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent"
               />
             </div>
